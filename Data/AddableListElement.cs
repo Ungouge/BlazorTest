7fea267a396eaa97ab9d2d314d76a1d3088786dd
Ungouge <zlobik89@gmail.com>
2020-08-11 23:50:27 +0200

    Add Addable List.

---
+++ Data/AddableListElement.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTest.Data
+{
+    public class AddableListElement
+    {        
+        public string Text { get; set; }
+    }
+}
---
+++ Data/AddableListService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTest.Data
+{
+    public class AddableListService
+    {
+        private readonly IList<AddableListElement> _addableElementsList = new List<AddableListElement>();
+
+        public Task<IList<AddableListElement>> GetElementListAsync()
+        {
+            return Task.FromResult(_addableElementsList);
+        }
+    }
+}
